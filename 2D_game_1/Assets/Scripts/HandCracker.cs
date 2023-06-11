0dda16d9ed5530f8c498c22d33a2e3924cdf0e7f
ThiharaKumarasinghe <thihara.kumarasinghe@gmail.com>
2023-06-11 19:27:25 +0530

    game1 initial
    Game 1:
    start to upload to the GIT hub

---
+++ 2D_game_1/Assets/Scripts/HandCracker.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandCracker : MonoBehaviour
+{
+
+    public UDPReceive UDP_Receiver;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //string data = UDP_Receiver.data;
+       /*data = data.Remove(0, 1);
+        data = data.Remove(data.Length - 1, 1);*/
+
+
+
+
+        /*print(data);
+        print("hello");
+        string[] points = data.Split(',');
+        print(points[0]);*/
+
+
+
+        /*int a = Convert.ToInt32(points[0]);
+        print(a);*/
+        //float x = 1;
+        //float x = float.Parse(points[0*3]) / 1000;
+
+        //print(x);
+
+
+        //int temp = phase(points[0]);
+        //int data1 = data[1];
+        //int data2 = data[1];
+
+
+        //transform.position = (new Vector3(0, 0, 0));
+
+
+
+
+    }
+}
---
+++ 2D_game_1/Assets/Scripts/PlyerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlyerController : MonoBehaviour
+{
+
+    private Rigidbody2D rbody;
+    public float speed=100 ;
+    public float count;
+    public TextMeshProUGUI countText;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        rbody.AddForce(new Vector2(horizontalInput, verticalInput)*speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.gameObject.CompareTag("pickUp"))
+        {
+            Destroy(target.gameObject);
+            countShow();
+        }
+    }
+
+    void countShow()
+    {
+        countText.text = count.ToString();
+        count++;
+
+    }
+
+}
+
+
